d4d0e3494760ff85123dfb5d898c7b8199632a1a
Theo <69400012+Teschl@users.noreply.github.com>
2024-08-06 16:35:04 +0200

    Rework example gallery (#58)
    * Add test reference to readme
    
    * Add custom thumbnail support
    
    * Rework example notebooks
    
    * Add  name arg to gen_random

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
 
@@ -21,7 +13,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'nbsphinx',
-    'myst_parser',
 ]
 
 project = 'TopoToolbox'
@@ -30,14 +21,11 @@ author = 'TopoToolbox Team'
 release = '3.0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
@@ -57,9 +45,17 @@ autodoc_default_options = {
     'members': True,
     'undoc-members': True,
     'private-members': False,
-    'special-members': '__init__',
     'inherited-members': True,
     'show-inheritance': True,
 }
 
 autosummary_generate = True  # Enable autosummary to generate stub files
+
+# -- Options for nbgallery ---------------------------------------------------
+
+nbsphinx_thumbnails = {
+    '_temp/excesstopography': '_static/thumbnails/placeholder.png',
+    '_temp/magicfunctions': '_static/thumbnails/placeholder.png',
+    '_temp/plotting': '_static/thumbnails/placeholder.png',
+    '_temp/downloading': '_static/thumbnails/placeholder.png',
+}
