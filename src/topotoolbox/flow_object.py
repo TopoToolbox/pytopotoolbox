bcf8838b207c842bf36ca98b30cd627ff096fba9
Teschl <69400012+Teschl@users.noreply.github.com>
2024-08-16 15:29:52 +0200

    Add doc strings

--- src/topotoolbox/flow_object.py
+++ src/topotoolbox/flow_object.py
@@ -1,12 +1,11 @@
 """This module contains the FlowObject class.
 """
-
-import copy
+from .grid_object import GridObject
 
 import numpy as np
 import matplotlib.pyplot as plt
-from .grid_object import *
 
+# Add future flow functions here
 # pylint: disable=import-error
 # from ._flow import (  # type: ignore
 #    flow_somefuncname
@@ -26,7 +25,24 @@ __all__ = ['FlowObject']
 
 
 class FlowObject():
+    """A class containing containing (water-) flow information about a given
+    digital elevation model (DEM).
+    """
+
     def __init__(self, grid: GridObject):
+        """The constructor for the FlowObject. Takes a GridObject as input,
+        computes flow direction information and saves them as an FlowObject.
+
+        Parameters
+        ----------
+        grid : GridObject
+            The GridObject that will be the basis of the computation.
+
+        Notes
+        -----
+        Large intermediate arrays are created during the initialization
+        process, which could lead to issues when using very large DEMs.
+        """
         dims = grid.shape
         dem = grid.z
 
@@ -40,23 +56,21 @@ class FlowObject():
         conncomps = np.zeros_like(dem, dtype=np.int64, order='F')
         grid_gwdt_computecosts(costs, conncomps, flats, dem, filled_dem, dims)
 
-        dist = np.zeros_like(flats, dtype=np.float32)
-        # prev = np.zeros_like(flats, dtype=np.int64)
-        prev = conncomps
-        heap = np.zeros_like(flats, dtype=np.int64)
-        back = np.zeros_like(flats, dtype=np.int64)
+        dist = np.zeros_like(flats, dtype=np.float32, order='F')
+        prev = conncomps  # prev: dtype=np.int64
+        heap = np.zeros_like(flats, dtype=np.int64, order='F')
+        back = np.zeros_like(flats, dtype=np.int64, order='F')
         grid_gwdt(dist, prev, costs, flats, heap, back, dims)
 
-        # source = np.zeros_like(dem, dtype=np.int64, order='F')
-        source = heap
+        source = heap  # source: dtype=np.int64
         direction = np.zeros_like(dem, dtype=np.uint8, order='F')
         grid_flow_routing_d8_carve(
             source, direction, filled_dem, dist, flats, dims)
 
-        # target = np.zeros_like(dem, dtype=np.int64, order='F')
-        target = back
+        target = back  # target: dtype=int64
         grid_flow_routing_targets(target, source, direction, dims)
-        # TODO: use del to delete unused large arrays?
+
+        # TODO: use 'del' to immediately delete unused large arrays?
 
         self.path = grid.path
         self.name = grid.name
@@ -71,3 +85,29 @@ class FlowObject():
         self.bounds = grid.bounds
         self.transform = grid.transform
         self.crs = grid.crs
+
+    def show(self, cmap: str = 'terrain'):
+        """
+        Display the FlowObject instance as an image using Matplotlib.
+
+        Parameters
+        ----------
+        cmap : str, optional
+            Matplotlib colormap that will be used in the plot.
+        """
+
+        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+
+        axs[0].imshow(self.z, cmap=cmap)
+        axs[0].set_title('dem')
+
+        axs[1].imshow(self.target, cmap=cmap)
+        axs[1].set_title('target')
+
+        axs[2].imshow(self.source, cmap=cmap)
+        axs[2].set_title('source')
+
+        plt.tight_layout()
+        plt.show()
+
+    # TODO: Add magic functions, maybe use mixins to reuse GridObject functions
