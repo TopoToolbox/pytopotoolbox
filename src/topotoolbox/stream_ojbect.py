e39ae3470d23f8515fdce28a2028bfe727302900
Teschl <69400012+Teschl@users.noreply.github.com>
2024-09-04 13:09:16 +0200

    Setup StreamObject

---
+++ src/topotoolbox/stream_ojbect.py
@@ -0,0 +1,17 @@
+"""This module contains the StreamObject class.
+"""
+
+import copy
+
+import numpy as np
+import matplotlib.pyplot as plot
+
+# pylint: disable=import-error
+from ._stream import (  # type: ignore
+    stream_placeholder,
+)
+
+
+class StreamObject():
+    def __init__(self) -> None:
+        pass
