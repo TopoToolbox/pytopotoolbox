ac38c131e848d8b6eecc62aaa97a8e33c1af8520
Teschl <69400012+Teschl@users.noreply.github.com>
2024-09-21 17:43:36 +0200

    Implement StreamObject

--- src/topotoolbox/stream_object.py
+++ src/topotoolbox/stream_object.py
@@ -20,42 +20,46 @@ class StreamObject():
                  threshold: int | float | GridObject | np.ndarray = 1) -> None:
 
         if units not in ['pixels', 'mapunits', 'm2', 'km2']:
-            # TODO: ADD ERROR MESSAGE
-            err = ""
+            err = (f"Invalid unit '{units}' provided. Expected one of "
+                   f"'pixels', 'mapunits', 'm2', 'km2'.")
             raise ValueError(err) from None
 
         if isinstance(threshold, int) or isinstance(threshold, float):
             threshold = np.full(flow.shape, threshold)
         elif isinstance(threshold, np.ndarray):
             if threshold.shape != flow.shape:
-                # TODO: ADD ERROR MESSAGE
-                err = ""
+                err = (f"Threshold array shape {threshold.shape} does not "
+                       f"match flow shape {flow.shape}.")
                 raise ValueError(err) from None
-            threshold = threshold(np.float32, order='F')
+            threshold = threshold.astype(np.float32, order='F')
         else:
             if threshold.shape != flow.shape:
-                # TODO: ADD ERROR MESSAGE
-                err = ""
+                err = (f"Threshold GridObject shape {threshold.shape} does "
+                       f"not match flow shape {flow.shape}.")
                 raise ValueError(err) from None
 
-            threshold = threshold.z(np.float32, order='F')
+            threshold = threshold.z.astype(np.float32, order='F')
 
+        # Generate the flow accumulation matrix (acc)
         acc = np.zeros_like(flow.target, order='F', dtype=np.float32)
         weights = np.ones_like(flow.target, order='F', dtype=np.float32)
-
         _flow.flow_accumulation(
             acc, flow.source, flow.direction, weights, flow.shape)
 
-        temp = []
+        # Generate a 1D array that holds all indexes where more water than
+        # in the required threshold is collected. (acc >= threshold)
         threshold = threshold.flatten(order='F')
         acc = acc.flatten(order='F')
+        temp = []
         for i, value in enumerate(acc):
             if value >= threshold[i]:
                 temp.append(i)
 
         self.stream = np.array(temp, dtype=np.int32)
 
-        # raster metadata
+        # Based on the stream array, generate 3 1D arrays where the value of
+        # the stream array at each index holds respective value of the
+        # original array. (source, target and direction)
         source = flow.source.flatten(order='F')
         temp_source = []
         target = flow.target.flatten(order='F')
@@ -82,26 +86,44 @@ class StreamObject():
         self.transform = flow.transform
         self.crs = flow.crs
 
-    def show(self, cmap='binary') -> None:
+    def show(self, cmap='hot', overlay: GridObject | None = None,
+             overlay_cmap: str = 'binary', alpha: float = 0.8) -> None:
         """
         Display the StreamObject instance as an image using Matplotlib.
 
         Parameters
         ----------
         cmap : str, optional
-            Matplotlib colormap that will be used in the plot.
+            Matplotlib colormap that will be used for the stream.
+        overlay_cmap : str, optional
+            Matplotlib colormap that will be used in the background plot.
+        overlay : GridObject | None, optional
+            To overlay the stream over a dem to better visualize the stream.
+        alpha : float, optional
+            When using an dem to overlay, this controls the opacity of the dem.
         """
         stream = np.zeros(shape=self.shape, dtype=np.int32, order='F')
         for i in self.stream:
-            x = i // self.shape[0]
-            y = i % self.shape[0]
+            x = i % self.shape[0]
+            y = i // self.shape[0]
             stream[x][y] = 1
 
-        plt.imshow(stream, cmap=cmap)
-        plt.title(self.name)
-        plt.colorbar()
-        plt.tight_layout()
-        plt.show()
+        if overlay is not None:
+            if self.shape == overlay.shape:
+                plt.imshow(overlay, cmap=overlay_cmap, alpha=alpha)
+                plt.imshow(stream, cmap=cmap,
+                           alpha=stream.astype(np.float32))
+                plt.show()
+            else:
+                err = (f"Shape mismatch: Stream shape {self.shape} does not "
+                       f"match overlay shape {overlay.shape}.")
+                raise ValueError(err) from None
+        else:
+            plt.imshow(stream, cmap=cmap)
+            plt.title(self.name)
+            plt.colorbar()
+            plt.tight_layout()
+            plt.show()
 
     # 'Magic' functions:
     # ------------------------------------------------------------------------
