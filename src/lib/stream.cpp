d6feea8800668ff14e203620798039fe0366a1c0
William Kearney <william.kearney@uni-potsdam.de>
2024-11-12 15:01:08 +0100

    Wrap streamquad_trapz_*
    This commit adds wrapper functions to the `_stream`
    module (src/lib/stream.cpp) for `streamquad_trapz_f32` and
    `streamquad_trapz_f64`, which integrate along a stream network and
    makes the appropriate changes in CMakeLists.txt to compile these
    functions.
    
    These functions are going to be most useful in implementing
    higher-level functions like the chi transform (#75), and should
    probably not be depended upon directly by users.
    
    Note that libtopotoolbox's `streamquad_trapz_f32` and
    `streamquad_trapz_f64` require node attribute lists that have 1-based
    indexing (corresponding to MATLAB), while pytopotoolbox's
    `StreamObject` has 0-based node attribute lists to make working with
    them in Python easier. Python edge attribute lists like
    `StreamObject.source` and `StreamObject.target` should be incremented
    by 1 before passing as the wrapper functions' `source` and `target`
    arguments. This may change in the future.
    
    This commit itself does not depend on the fixes to StreamObject node
    numbering in #78, but the `streamquad_trapz_*` wrappers will not work
    properly (i.e. will attempt to access out-of-bounds memory) until #78
    is merged.

--- src/lib/stream.cpp
+++ src/lib/stream.cpp
@@ -1,20 +1,57 @@
 // This file contains bindings for the stream functions
 
 // It is expected, that the #include statemnts can raise errors in your IDE.
-// The needed files for those imports are only provided during the build process.
+// The needed files for those imports are only provided during the build
+// process.
 
 extern "C" {
-    #include <topotoolbox.h>
+#include <topotoolbox.h>
 }
 
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-// Make wrap_funcname() function available as grid_funcname() to be used by
+// Make wrap_funcname() function available as stream_funcname() to be used by
 // by functions in the pytopotoolbox package
 
-PYBIND11_MODULE(_stream, m) {
+void wrap_streamquad_trapz_f32(py::array_t<float> integral,
+                               py::array_t<float> integrand,
+                               py::array_t<ptrdiff_t> source,
+                               py::array_t<ptrdiff_t> target,
+                               py::array_t<float> weight) {
 
+  float *integral_ptr = integral.mutable_data();
+  float *integrand_ptr = integrand.mutable_data();
+  ptrdiff_t *source_ptr = source.mutable_data();
+  ptrdiff_t *target_ptr = target.mutable_data();
+  float *weight_ptr = weight.mutable_data();
+
+  ptrdiff_t edge_count = source.size();
+
+  streamquad_trapz_f32(integral_ptr, integrand_ptr, source_ptr, target_ptr,
+                       weight_ptr, edge_count);
+}
+
+void wrap_streamquad_trapz_f64(py::array_t<double> integral,
+                               py::array_t<double> integrand,
+                               py::array_t<ptrdiff_t> source,
+                               py::array_t<ptrdiff_t> target,
+                               py::array_t<float> weight) {
+  double *integral_ptr = integral.mutable_data();
+  double *integrand_ptr = integrand.mutable_data();
+  ptrdiff_t *source_ptr = source.mutable_data();
+  ptrdiff_t *target_ptr = target.mutable_data();
+  float *weight_ptr = weight.mutable_data();
+
+  ptrdiff_t edge_count = source.size();
+
+  streamquad_trapz_f64(integral_ptr, integrand_ptr, source_ptr, target_ptr,
+                       weight_ptr, edge_count);
+}
+
+PYBIND11_MODULE(_stream, m) {
+  m.def("streamquad_trapz_f32", &wrap_streamquad_trapz_f32);
+  m.def("streamquad_trapz_f64", &wrap_streamquad_trapz_f64);
 }
