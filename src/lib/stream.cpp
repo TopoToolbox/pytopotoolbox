c69e50c5941481ee290e36459149054a2cf0298b
William Kearney <william.kearney@uni-potsdam.de>
2025-02-20 15:34:40 +0100

    Implement klargestconncomps (#155)
    This version of `klargestconncomps` uses traversals implemented in
    libtopotoolbox to compute the size of each connected component of the
    stream network and to propagate the identities of the largest
    connected components upstream.
    
    test/stream_object.py adds some tests that ensure that `trunk` and
    `klargestconncomps` create subgraphs of the original stream network. A
    helper function `issubgraph` could also be included in the
    `StreamObject` interface, but it is not particularly efficient as it
    relies on set operations.

--- src/lib/stream.cpp
+++ src/lib/stream.cpp
@@ -83,9 +83,53 @@ void wrap_traverse_down_f32_max_add(py::array_t<float> output,
                             target_ptr, edge_count);
 }
 
+void wrap_traverse_down_f32_add_mul(py::array_t<float> output,
+                                    py::array_t<float> input,
+                                    py::array_t<ptrdiff_t> source,
+                                    py::array_t<ptrdiff_t> target) {
+  float *output_ptr = output.mutable_data();
+  float *input_ptr = input.mutable_data();
+  ptrdiff_t *source_ptr = source.mutable_data();
+  ptrdiff_t *target_ptr = target.mutable_data();
+
+  ptrdiff_t edge_count = source.size();
+
+  traverse_down_f32_add_mul(output_ptr, input_ptr, source_ptr,
+                            target_ptr, edge_count);
+}
+
+void wrap_edgelist_degree(py::array_t<uint8_t> indegree,
+                          py::array_t<uint8_t> outdegree,
+                          py::array_t<ptrdiff_t> source,
+                          py::array_t<ptrdiff_t> target) {
+  uint8_t *indegree_ptr = indegree.mutable_data();
+  uint8_t *outdegree_ptr = outdegree.mutable_data();
+  ptrdiff_t *source_ptr = source.mutable_data();
+  ptrdiff_t *target_ptr = target.mutable_data();
+
+  ptrdiff_t node_count = indegree.size();
+  ptrdiff_t edge_count = source.size();
+
+  edgelist_degree(indegree_ptr, outdegree_ptr, source_ptr,
+                  target_ptr, node_count, edge_count);
+}
+
+void wrap_propagatevaluesupstream_u8(py::array_t<uint8_t> data,
+                                     py::array_t<ptrdiff_t> source,
+                                     py::array_t<ptrdiff_t> target) {
+
+  propagatevaluesupstream_u8(data.mutable_data(),
+                             source.mutable_data(),
+                             target.mutable_data(),
+                             source.size());
+}
+
 PYBIND11_MODULE(_stream, m) {
   m.def("streamquad_trapz_f32", &wrap_streamquad_trapz_f32);
   m.def("streamquad_trapz_f64", &wrap_streamquad_trapz_f64);
   m.def("traverse_up_u32_and", &wrap_traverse_up_u32_and);
-  m.def("traverse_down_f32_max_add", &wrap_traverse_down_f32_max_add);  
+  m.def("traverse_down_f32_max_add", &wrap_traverse_down_f32_max_add);
+  m.def("traverse_down_f32_add_mul", &wrap_traverse_down_f32_add_mul);
+  m.def("edgelist_degree", &wrap_edgelist_degree);
+  m.def("propagatevaluesupstream_u8", &wrap_propagatevaluesupstream_u8);
 }
