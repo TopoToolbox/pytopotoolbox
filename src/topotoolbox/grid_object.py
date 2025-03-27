650406364c42c5e3b38cb517e6d5be48d66797d9
William Kearney <william.kearney@uni-potsdam.de>
2025-03-27 14:49:22 +0100

    Catch errors in older versions of Numpy

--- src/topotoolbox/grid_object.py
+++ src/topotoolbox/grid_object.py
@@ -1389,7 +1389,14 @@ class GridObject():
         self.z[index] = value
 
     def __array__(self, dtype=None, copy=None):
-        return np.array(self.z, dtype=dtype, copy=copy)
+        try:
+            # If we are using Numpy v1.x, this will copy-if-needed
+            # when copy=False.
+            return np.array(self.z, dtype=dtype, copy=copy)
+        except:
+            # If np.array fails because copy=None and we are using an
+            # older version of Numpy, use asarray to copy-if-needed.
+            return np.asarray(self.z, dtype=dtype)
 
     def __str__(self):
         return str(self.z)
