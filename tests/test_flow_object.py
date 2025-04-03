a14e3e03a6e4fc0c7a14985feac31ca59587b29e
William Kearney <william.kearney@uni-potsdam.de>
2025-04-03 08:43:45 +0200

    Add validate_alignment tests
    These tests make sure that topotoolbox functions copy the appropriate
    metadata into their outputs.

--- tests/test_flow_object.py
+++ tests/test_flow_object.py
@@ -12,6 +12,8 @@ def test_flowobject(wide_dem):
     original_dem = dem.z.copy()
     fd = topo.FlowObject(dem);
 
+    assert topo.validate_alignment(dem, fd)
+
     # Ensure that the source and target arrays contain valid pixel indices
     assert np.all((0 <= fd.source) & (fd.source < np.prod(dem.shape)))
     assert np.all((0 <= fd.target) & (fd.target < np.prod(dem.shape)))
@@ -19,15 +21,23 @@ def test_flowobject(wide_dem):
     # Run flow_accumulation at least once during the tests
     acc = fd.flow_accumulation()
 
+    assert topo.validate_alignment(fd, acc)
+
     # Ensure that FlowObject does not modify the original DEM
     assert np.all(dem.z == original_dem)
 
 def test_flowpathextract(wide_dem):
     fd = topo.FlowObject(wide_dem)
     s = topo.StreamObject(fd)
+
+    assert topo.validate_alignment(wide_dem, fd)
+    assert topo.validate_alignment(wide_dem, s)
+
     ch = s.streampoi('channelheads')
 
     s2 = topo.StreamObject(fd, channelheads=s.stream[ch][0:1])
+
+    assert topo.validate_alignment(wide_dem, s2)
 
     idxs = fd.flowpathextract(s.stream[ch][0])
     assert np.array_equal(s2.stream, idxs)
--- tests/test_stream_object.py
+++ tests/test_stream_object.py
@@ -66,8 +66,17 @@ def test_streamobject_sizes(tall_dem, wide_dem):
     tall_flow = topo.FlowObject(tall_dem)
     tall_stream = topo.StreamObject(tall_flow)
 
+    assert topo.validate_alignment(tall_dem, tall_flow)
+    assert topo.validate_alignment(tall_dem, tall_stream)
+
     wide_flow = topo.FlowObject(wide_dem)
     wide_stream = topo.StreamObject(wide_flow)
+
+    assert topo.validate_alignment(wide_dem, wide_flow)
+    assert topo.validate_alignment(wide_dem, wide_stream)
+
+    assert not topo.validate_alignment(wide_dem, tall_flow)
+    assert not topo.validate_alignment(wide_dem, tall_stream)
 
     assert (wide_stream.target.size == wide_stream.source.size ==
             wide_stream.direction.size)
@@ -106,6 +115,10 @@ def test_stream_subgraphs(tall_dem, wide_dem):
     assert issubgraph(tall_k1_trunk, tall_k1)
     assert not issubgraph(tall_trunk, tall_k1)
 
+    assert topo.validate_alignment(tall_trunk, tall_stream)
+    assert topo.validate_alignment(tall_k1, tall_stream)
+    assert topo.validate_alignment(tall_k1_trunk, tall_stream)
+
     wide_flow = topo.FlowObject(wide_dem)
     wide_stream = topo.StreamObject(wide_flow)
 
@@ -118,6 +131,10 @@ def test_stream_subgraphs(tall_dem, wide_dem):
     assert issubgraph(wide_k1_trunk, wide_stream)
     assert issubgraph(wide_k1_trunk, wide_k1)
     assert not issubgraph(wide_trunk, wide_k1)
+
+    assert topo.validate_alignment(wide_trunk, wide_stream)
+    assert topo.validate_alignment(wide_k1, wide_stream)
+    assert topo.validate_alignment(wide_k1_trunk, wide_stream)
 
     assert not issubgraph(wide_trunk, tall_stream)
     assert not issubgraph(tall_trunk, wide_stream)
@@ -213,19 +230,27 @@ def test_stream_channelheads(tall_dem, wide_dem):
     fd = topo.FlowObject(tall_dem)
     s = topo.StreamObject(fd)
 
+    assert topo.validate_alignment(fd, s)
+
     channel_heads = s.streampoi("channelheads")
 
     s2 = topo.StreamObject(fd, channelheads=s.stream[channel_heads])
+
+    assert topo.validate_alignment(fd, s2)
 
     assert np.array_equal(s2.stream[s2.source], s.stream[s.source])
     assert np.array_equal(s2.stream[s2.target], s.stream[s.target])
 
     fd = topo.FlowObject(wide_dem)
     s = topo.StreamObject(fd)
+
+    assert topo.validate_alignment(fd, s)
     
     channel_heads = s.streampoi("channelheads")
 
     s2 = topo.StreamObject(fd, channelheads=s.stream[channel_heads])
+
+    assert topo.validate_alignment(fd, s2)
 
     assert np.array_equal(s2.stream[s2.source], s.stream[s.source])
     assert np.array_equal(s2.stream[s2.target], s.stream[s.target])
