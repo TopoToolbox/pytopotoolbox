7eb165b4f604744a6c8b9c61e9d91df6cc56bbd1
Teschl <69400012+Teschl@users.noreply.github.com>
2025-05-09 11:37:46 +0200

    Remove everything but zscore

--- src/topotoolbox/grid_object.py
+++ src/topotoolbox/grid_object.py
@@ -1252,12 +1252,6 @@ class GridObject():
         result.z = (self.z - np.nanmean(self.z)) / np.nanstd(self.z)
         return result
 
-    def castshadows(self, azimuth, altitude) -> 'GridObject':
-        pass
-
-    def impaintnans():
-        pass
-
         # 'Magic' functions:
         # ------------------------------------------------------------------------
 
