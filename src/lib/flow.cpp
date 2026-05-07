1418708db954130555b9825422384702f6ae4360
William Kearney <william.kearney@uni-potsdam.de>
2026-05-07 13:49:16 +0200

    Wrap and test flow_routing_tsort (#426)
    See #421
    
    This commit adds a binding for the new topological sorting routine in
    libtopotoolbox. A test is added to make sure that it does indeed
    topologically sort the edge list and that the edge weights get put in
    the right location. The synthetic edgesets used in the tests are
    filtered so that they formed a directed acyclic graph.
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

--- src/lib/flow.cpp
+++ src/lib/flow.cpp
@@ -1,9 +1,9 @@
 extern "C" {
-  #include <topotoolbox.h>
+#include <topotoolbox.h>
 }
 
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
@@ -34,14 +34,40 @@ ptrdiff_t wrap_edgeset_merge(py::array_t<float> outweights,
   ptrdiff_t dims[2] = {bitmap1.shape(0), bitmap1.shape(1)};
   return edgeset_merge(outweights.mutable_data(), outscan.mutable_data(),
                        bitmap1.mutable_data(), weights1.mutable_data(),
-                       bitmap2.mutable_data(), weights2.mutable_data(),
-                       dims);
+                       bitmap2.mutable_data(), weights2.mutable_data(), dims);
 }
 
+void wrap_flow_routing_tsort(
+    py::array_t<ptrdiff_t> stream, py::array_t<ptrdiff_t> source,
+    py::array_t<ptrdiff_t> target, py::array_t<float> sorted_weight,
+    py::array_t<ptrdiff_t> stack, py::array_t<uint8_t> stackdir,
+    py::array_t<uint8_t> direction, py::array_t<float> weight,
+    py::array_t<ptrdiff_t> weightscan, py::array_t<uint8_t> visited) {
+
+  ptrdiff_t edge_count = weight.size();
+
+  int order = direction.flags() & py::array::c_style ? 1 : 0;
+
+  ptrdiff_t dims[2] = {weightscan.shape(0), weightscan.shape(1)};
+  // Reverse the dimensions if in row-major order
+  if (order) {
+    dims[0] = weightscan.shape(1);
+    dims[1] = weightscan.shape(0);
+  }
+
+
+  flow_routing_tsort(stream.mutable_data(), source.mutable_data(),
+                     target.mutable_data(), sorted_weight.mutable_data(),
+                     stack.mutable_data(), stackdir.mutable_data(),
+                     direction.mutable_data(), weight.mutable_data(),
+                     weightscan.mutable_data(), visited.mutable_data(),
+                     edge_count, dims, order);
+}
 
 PYBIND11_MODULE(_flow, m) {
   m.def("edgeset_count", &wrap_edgeset_count);
   m.def("edgeset_scan", &wrap_edgeset_scan);
   m.def("edgeset_count_merged", &wrap_edgeset_count_merged);
   m.def("edgeset_merge", &wrap_edgeset_merge);
+  m.def("flow_routing_tsort", &wrap_flow_routing_tsort);
 }
