1d109b6f3ad874a7a5fd225f2e51781ba24c6105
William Kearney <william.kearney@uni-potsdam.de>
2026-05-12 16:17:09 +0200

    Add D8 flow routing using new libtopotoolbox interface (#432)
    Resolves #421
    
    This commit allows users to use the new libtopotoolbox functions for
    D8 routing with least cost auxiliary topography carving by passing
    `method="d8"` and `sink_resolution="lcat"` to the FlowObject
    constructor.
    
    The default argument for "sink_resolution" is "carve", which applies
    the previous behavior.
    
    The new version (`FlowObject(method="d8", sink_resolution="lcat")`)
    will NOT produce identical results to (`FlowObject(method="d8",
    sink_resolution="carve")`) because of differences in the underlying
    libtopotoolbox implementation.
    
    All of the FlowObject tests have been modified to also run with "d8"
    and "lcat", and they should pass.
    
    The various flow routing functions are implemented as private
    functions of the flow_object module. Users should not call these
    functions but use the `FlowObject` constructor. It is highly likely
    that the implementation will need to change as we add different flow
    routing algorithms (Dinf, MFD, etc.), but the FlowObject constructor
    will always be the user-facing interface to flow routing.
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

--- tests/test_edgeset.py
+++ tests/test_edgeset.py
@@ -9,7 +9,10 @@ rng = np.random.default_rng(210057042403268582692858923958297081890)
 @pytest.fixture
 def dem():
     sz = (11, 13)
-    return rng.random(sz, dtype=np.float32)
+    z = tt3.GridObject()
+    z.z = rng.random(sz, dtype=np.float32)
+    z.cellsize = 1.0
+    return z
 
 def bitmap(z):
     sz = z.shape
@@ -89,91 +92,3 @@ def test_edgeset_tsort(edgeset1):
     # first outgoing edge of each source. This may not be the same
     # edge, but it should have the same weight.
     assert np.array_equal(sweight, edgeset1.weights[edgeset1.scan[np.unravel_index(source, edgeset1.shape)]])
-
-def test_flow_routing_d8(dem):
-    directions = np.zeros(dem.shape, dtype=np.uint8)
-    tt3._flow.flow_routing_d8_directions(directions, dem)
-
-    assert np.all(np.bitwise_count(directions) < 2)
-
-    scan = np.zeros(dem.shape, dtype=np.int64)
-    c = tt3._flow.edgeset_scan(scan, directions)
-
-    assert c > 0
-
-    weights = np.zeros(c, dtype=np.float32)
-    tt3._flow.flow_routing_d8_weights(weights)
-
-    edges = tt3.EdgeSet(directions, scan, weights)
-
-    (stream, source, target, sweight) = edges.tsort()
-
-    assert np.all(sweight == 1.0)
-
-    visited = np.zeros_like(directions)
-    for (u, v) in zip(source, target):
-        visited[np.unravel_index(u, dem.shape)] += 1
-
-        assert visited[np.unravel_index(v, dem.shape)] == 0
-
-def test_resolve_flats_lcat(dem):
-    dims = (dem.shape[1], dem.shape[0])
-    demf = np.zeros(dem.shape, dtype=np.float32)
-    bc = np.ones_like(dem, dtype=np.uint8)
-    bc[1:-1, 1:-1] = 0
-
-    tt3._grid.fillsinks(demf, dem, bc, dims)
-
-    flats = np.zeros_like(dem, dtype=np.int32)
-    tt3._grid.identifyflats(flats, demf, dims)
-
-    costs = np.zeros_like(dem, dtype=np.float32)
-    conncomps = np.zeros_like(dem, dtype=np.int64)
-    tt3._grid.gwdt_computecosts(costs, conncomps, flats, dem, demf, dims)
-
-    aux  = np.zeros_like(flats, dtype=np.float32)
-    prev = np.zeros_like(flats, dtype=np.int64)
-    heap = np.zeros_like(flats, dtype=np.int64)
-    back = np.zeros_like(flats, dtype=np.int64)
-    tt3._grid.gwdt(aux, prev, costs, flats, heap, back, dims)
-
-    direction = np.zeros(dem.shape, dtype=np.uint8)
-    tt3._flow.flow_routing_d8_directions(direction, demf)
-
-    scan = np.zeros(dem.shape, dtype=np.int64)
-    c = tt3._flow.edgeset_scan(scan, direction)
-
-    weights = np.zeros(c, dtype=np.float32)
-    tt3._flow.flow_routing_d8_weights(weights)
-
-    e1 = tt3.EdgeSet(direction, scan, weights)
-
-    rdirection = np.zeros(dem.shape, dtype=np.uint8)
-    resolved = flats & 1 == 0
-
-    assert not np.all(resolved)
-
-    tt3._flow.resolve_flats_lcat(rdirection, resolved, aux, demf)
-
-    rscan = np.zeros(dem.shape, dtype=np.int64)
-    cr = tt3._flow.edgeset_scan(rscan, rdirection)
-
-    rweights = np.zeros(cr, dtype=np.float32)
-    tt3._flow.resolve_flats_lcat_weights(rweights)
-
-    e2 = tt3.EdgeSet(rdirection, rscan, rweights)
-
-    em = e1.merge(e2)
-
-    (stream, source, target, sweight) = em.tsort()
-
-    assert np.all(sweight == 1.0)
-
-    visited = np.zeros_like(direction)
-    for (u, v) in zip(source, target):
-        visited[np.unravel_index(u, dem.shape)] += 1
-
-        assert visited[np.unravel_index(v, dem.shape)] == 0
-
-    # All pixels with no outgoing edges should be on the boundary
-    assert np.all(bc[visited == 0] == 1)
